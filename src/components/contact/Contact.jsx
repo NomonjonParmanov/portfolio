0278014dad0a810f75c14d8b7ee654a80ef64c38
Nomonjonprog <ibragimovganijon354@gmail.com>
2024-06-29 20:06:47 +0500

    finished

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { PatternFormat } from "react-number-format";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mess, setMess] = useState("");
+  const BOT_TOKEN = "6765319054:AAGM7Q5sdKWUSY2ShxsGEu__RjKjaVuAyTA";
+  const CHAT_ID = "-1002101256953";
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let text = "Xaridor  %0A%0A";
+    text += `Name: ${name} %0A`;
+    text += `email: ${email} %0A`;
+    text += `koment: ${mess} %0A`;
+
+    let url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${text}`;
+    let api = new XMLHttpRequest();
+    api.open("GET", url, true);
+    api.send();
+    setName("");
+    setEmail("");
+    setMess("");
+  };
   return (
     <div className="contact">
       <div className="container">
@@ -9,22 +30,41 @@ const Contact = () => {
         <div className="message">
           <p>Send me a message</p>
         </div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="inps">
             <div className="inp">
               <label htmlFor="">Your name *</label>
-              <input type="text" placeholder="Enter your name" />
+              <input
+                required
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter your name"
+              />
             </div>
             <div className="inp">
-              <label htmlFor="">Your email *</label>
-              <input type="text" placeholder="Enter your email" />
+              <label htmlFor="">Your number *</label>
+              <PatternFormat
+                format="+998 (##) ###-##-##"
+                allowEmptyFormatting
+                mask="_"
+                placeholder="Enter your number"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
           </div>
           <div className="inp">
             <label className="label" htmlFor="">
               Your message *
             </label>
-            <textarea placeholder="Enter your needs"></textarea>
+            <textarea
+              value={mess}
+              onChange={(e) => setMess(e.target.value)}
+              required
+              placeholder="Enter your needs"
+            ></textarea>
           </div>
           <button>Send Message</button>
         </form>
